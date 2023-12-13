b9d3c0ca43aa7ee9bdf7f4e9071f8df6b97cdfa5
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-13 14:40:25 +0530

    Color constant for App / Separate class for App created

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:e_commerce_app/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+// -- Use this Class to setup themes, initial Bindings, any animations and much more
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: EAppTheme.lightTheme,
+      darkTheme: EAppTheme.darkTheme,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
+import 'package:e_commerce_app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:e_commerce_app/utils/theme/theme.dart';
 
 void main() {
+  
   runApp(const App());
-}
-
-
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: EAppTheme.lightTheme,
-      darkTheme: EAppTheme.darkTheme
-    );
-  }
-}
+}  
