557fd956e349895b9ed660bb6bde26fa6340d085
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-13 18:23:56 +0530

    Created constant for image string and added images

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+class EImages {
+
+  // -- App Logos
+  static const String darkAppLogo = "assets/logos/DALPHA Logo Dark.png";
+  static const String lightAppLogo = "assets/logos/DALPHA Logo Light.png";
+
+
+  // -- Social Logos
+  static const String google = "assets/logos/google_Logo.png";
+  static const String facebook = "assets/logos/Facebook_Logo.png";
+}
