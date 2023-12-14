090347c584d3117d5c785b64784b71ddd8864e18
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-14 18:07:00 +0530

    Enums created

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,5 @@
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods { paypal, googlePay, applePay, visa, masterCard, credirtCard, paystack, razorPay, paytm }
