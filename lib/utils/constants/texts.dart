6f081daf4eab2475b3b54ca3527e6e5ef7a77f11
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-14 17:56:20 +0530

    Text Strings Added

---
+++ lib/utils/constants/texts.dart
@@ -0,0 +1,20 @@
+class EText {
+
+  // -- OnBoarding Texts
+  static const String onBoardingTitle1 = "Choose ypur product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+  
+
+  static const String onBoardingTitlesub1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
+  static const String onBoardingTitlesub2 = "For Seamless Transactions, Choose Your Payment Product Convenience, Our Priority!";
+  static const String onBoardingTitlesub3 = "From Our Doorstep to Yours - Swifth, Secure, and Contactless Delivery!";
+
+
+  // -- Home
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "D-ALPHA";
+
+
+
+}
