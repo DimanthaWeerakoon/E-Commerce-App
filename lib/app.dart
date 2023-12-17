685a338a577fd4491608f8abd3c266b052a7a048
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-17 11:43:52 +0530

    Native Splash Screen created

--- lib/app.dart
+++ lib/app.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.dark,
       theme: EAppTheme.lightTheme,
       darkTheme: EAppTheme.darkTheme,
     );
