f14db1f8e86d2acb9af009d669c8329c5b15da56
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-21 16:27:48 +0530

    Navigation Completed

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,6 @@
 import 'package:e_commerce_app/features/shop/screens/home/home.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -9,10 +11,13 @@ class NavigationMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(NavigationController());
+    final darkMode = EHelperFunctions.isDarkMode(context);
     return Scaffold(
       bottomNavigationBar: Obx(() => NavigationBar(
         height: 80,
         elevation: 0,
+        backgroundColor: darkMode ? EColors.black :Colors.white,
+        indicatorColor: darkMode ? EColors.white.withOpacity(0.1) : EColors.black.withOpacity(0.1),
         selectedIndex: controller.selectedIndex.value,
         onDestinationSelected: (index) => controller.selectedIndex.value = index,
         destinations: const [
