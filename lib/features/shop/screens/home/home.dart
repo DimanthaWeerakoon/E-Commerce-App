60036056ab7b68e95ad7a8a77982ad973e0c7eaf
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-22 10:07:07 +0530

    Home Page UI initiated

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,30 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: EColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      color: EColors.white.withOpacity(0.1)
+                    ),
+                  )
+                ],
+              )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
