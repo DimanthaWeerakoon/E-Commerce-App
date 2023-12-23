3054ca36ece1dd4360a5f44890b2eab78cfd9ff7
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-23 22:43:50 +0530

    Curved Edges added to home page UI

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -11,19 +12,22 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-                color: EColors.primary,
-                padding: const EdgeInsets.all(0),
-                child:  SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(top: -150, right: -250, child: ECircularContainer(backgroundColor: EColors.white.withOpacity(0.1),)),
-                      Positioned(top: 100, right: -300, child: ECircularContainer(backgroundColor: EColors.white.withOpacity(0.1),)),
-                      
-                      ],
-                  ),
-                ))
+            ClipPath(
+              clipper: ECustomCurvedEdges(),
+              child: Container(
+                  color: EColors.primary,
+                  padding: const EdgeInsets.all(0),
+                  child:  SizedBox(
+                    height: 400,
+                    child: Stack(
+                      children: [
+                        Positioned(top: -150, right: -250, child: ECircularContainer(backgroundColor: EColors.white.withOpacity(0.1),)),
+                        Positioned(top: 100, right: -300, child: ECircularContainer(backgroundColor: EColors.white.withOpacity(0.1),)),
+                        
+                        ],
+                    ),
+                  )),
+            )
           ],
         ),
       ),
