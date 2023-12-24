15c672e66c20fc6323c6f85fa4e9871f9f31ef0a
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-24 17:39:05 +0530

    AppBar Widget created

---
+++ lib/common/widgets/appbar/appbar.dart
@@ -0,0 +1,42 @@
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/device/device_utility.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class EAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const EAppbar({
+    super.key,
+    this.title,
+    this.actions,
+    this.leadingIcon,
+    this.leadingOnPressed,
+    this.showBackArrow = true,
+  });
+
+  final Widget? title;
+  final bool showBackArrow;
+  final IconData? leadingIcon;
+  final List<Widget>? actions;
+  final VoidCallback? leadingOnPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: ESizes.md),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        leading: showBackArrow
+            ? IconButton(onPressed: () => Get.back(), icon: const Icon(Iconsax.arrow_left))
+            : leadingIcon != null ? IconButton(onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+                                  : null,
+        title: title,
+        actions: actions,
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(EDeviceUtils.getAppBarHieght());
+}
