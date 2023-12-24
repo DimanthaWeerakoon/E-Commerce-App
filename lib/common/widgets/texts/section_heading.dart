98f8e2efae5c0007cf0da5544f420a42b8b051fe
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-25 01:29:20 +0530

    Heading Text Widget Created

---
+++ lib/common/widgets/texts/section_heading.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ESectionHeading extends StatelessWidget {
+  const ESectionHeading({
+    super.key,
+    this.onPressed,
+    this.textColor,
+    this.buttonTitle = 'View all',
+    required this.title,
+    this.showActionButton = true,
+  });
+
+  final Color? textColor;
+  final bool showActionButton;
+  final String title, buttonTitle;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .headlineSmall!
+              .apply(color: textColor),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        if (showActionButton)
+          TextButton(onPressed: () {}, child: Text(buttonTitle))
+      ],
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -13,21 +14,39 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            /// Header -- 
+            /// Header --
             EPrimaryHeaderContainer(
               child: Column(
                 children: [
                   /// -- AppBar
                   EHomeAppBar(),
-                  SizedBox(height: 5,),
-                  
+                  SizedBox(
+                    height: 5,
+                  ),
+
                   /// -- SearchBar
                   ESearchContainer(
                     text: 'Search in Store',
                   ),
-                  SizedBox(height: ESizes.spaceBtwSections,) 
-                  
+                  SizedBox(
+                    height: ESizes.spaceBtwItems,
+                  ),
+
                   /// -- Categories
+                  Padding(
+                      padding: EdgeInsets.only(left: ESizes.defaultSpace),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ESectionHeading(
+                            title: 'Popular Categories',
+                            showActionButton: false,
+                          ),
+                          SizedBox(
+                            height: ESizes.spaceBtwItems,
+                          ),
+                        ],
+                      ))
                 ],
               ),
             )
@@ -37,7 +56,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-
