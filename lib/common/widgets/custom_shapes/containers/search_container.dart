7ab533b75d673f77568f307a580e4985378882cc
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-25 14:46:58 +0530

    Wrapped search container with gesture detector

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -7,33 +7,37 @@ import 'package:iconsax/iconsax.dart';
 
 class ESearchContainer extends StatelessWidget {
   const ESearchContainer({
-    super.key, required this.text, this.icon = Iconsax.search_normal, this.showBackground = true, this.showBorder = true,
+    super.key, required this.text, this.icon = Iconsax.search_normal, this.showBackground = true, this.showBorder = true, this.onTap,
   });
 
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     final darkMode = EHelperFunctions.isDarkMode(context);
-    return Padding(
-      padding: const EdgeInsets.all(ESizes.defaultSpace),
-      child: Container(
-        width: EDeviceUtils.getScreenWidth(context),
-        padding: const EdgeInsets.all(ESizes.md),
-        decoration: BoxDecoration(
-          color: showBackground ? darkMode ? EColors.dark : EColors.light : Colors.transparent,
-          borderRadius: BorderRadius.circular(ESizes.cardRadiusLg),
-          border: showBorder ?  Border.all(color: EColors.darkerGrey) : null,
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(ESizes.defaultSpace),
+        child: Container(
+          width: EDeviceUtils.getScreenWidth(context),
+          padding: const EdgeInsets.all(ESizes.md),
+          decoration: BoxDecoration(
+            color: showBackground ? darkMode ? EColors.dark : EColors.light : Colors.transparent,
+            borderRadius: BorderRadius.circular(ESizes.cardRadiusLg),
+            border: showBorder ?  Border.all(color: EColors.darkerGrey) : null,
+          ),
+        child: Row(
+          children: [
+            Icon(icon, color: EColors.darkerGrey ,),
+            const SizedBox(width: ESizes.spaceBtwItems),
+            Text(text, style: Theme.of(context).textTheme.bodySmall)
+          ],
+        )  
         ),
-      child: Row(
-        children: [
-          Icon(icon, color: EColors.darkerGrey ,),
-          const SizedBox(width: ESizes.spaceBtwItems),
-          Text(text, style: Theme.of(context).textTheme.bodySmall)
-        ],
-      )  
       ),
     );
   }
