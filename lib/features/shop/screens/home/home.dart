21d08364b72502908d34961e21459b8df51089ff
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-25 14:30:50 +0530

    Categories Scroll View Created

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -2,7 +2,10 @@ import 'package:e_commerce_app/common/widgets/custom_shapes/containers/primary_h
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,7 +13,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -19,37 +22,116 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 children: [
                   /// -- AppBar
-                  EHomeAppBar(),
-                  SizedBox(
+                  const EHomeAppBar(),
+                  const SizedBox(
                     height: 5,
                   ),
 
                   /// -- SearchBar
-                  ESearchContainer(
+                  const ESearchContainer(
                     text: 'Search in Store',
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: ESizes.spaceBtwItems,
                   ),
 
                   /// -- Categories
                   Padding(
-                      padding: EdgeInsets.only(left: ESizes.defaultSpace),
+                      padding: const EdgeInsets.only(left: ESizes.defaultSpace),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          ESectionHeading(
+                          /// -- Heading
+                          const ESectionHeading(
                             title: 'Popular Categories',
                             showActionButton: false,
+                            textColor: EColors.white,
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: ESizes.spaceBtwItems,
                           ),
+
+                          /// -- Categories
+                          SizedBox(
+                            height: 80,
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: 6,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (_, index) {
+                                return EVerticalImageText(
+                                  image: EImages.clothIcon,
+                                  title: 'Dress',
+                                  onTap: () {},
+                                );
+                              },
+                            ),
+                          )
                         ],
                       ))
                 ],
               ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EVerticalImageText extends StatelessWidget {
+  const EVerticalImageText({
+    super.key,
+    required this.image,
+    required this.title,
+    this.textColor = EColors.white,
+    this.backgroundColor = EColors.white,
+    this.onTap,
+  });
+
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: ESizes.spaceBtwItems),
+        child: Column(
+          children: [
+            Container(
+                width: 56,
+                height: 56,
+                padding: const EdgeInsets.all(ESizes.sm),
+                decoration: BoxDecoration(
+                    color: backgroundColor ?? (EHelperFunctions.isDarkMode(context) ? EColors.black : EColors.white),
+                    borderRadius: BorderRadius.circular(100)),
+                child: Center(
+                    child: Image(
+                  image: AssetImage(image),
+                  fit: BoxFit.scaleDown,
+                  color: EColors.dark,
+                ))),
+
+            /// Text
+            const SizedBox(
+              height: ESizes.spaceBtwItems / 2,
+            ),
+            SizedBox(
+                width: 55,
+                child: Text(
+                  title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium!
+                      .apply(color: textColor),
+                   textAlign: TextAlign.center,   
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ))
           ],
         ),
       ),
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -20,4 +20,16 @@ class EImages {
   static const String verifyEmail1 = "assets/images/verify_email_images/verify_email_1.png";
   static const String verifyEmail2 = "assets/images/verify_email_images/verify_email_2.png";
   static const String resetPassword = "assets/images/verify_email_images/reset_password.png";
+
+
+  // -- Category icons
+  static const String sportIcon = "assets/icons/categories/icons8-basketball-64.png";
+  static const String clothIcon = "assets/icons/categories/icons8-jumper-64.png";
+  static const String electronicIcon = "assets/icons/categories/icons8-drill-64.png";
+  static const String toyIcon = "assets/icons/categories/icons8-perfume-bottle-64.png";
+  static const String jeweleryIcon = "assets/icons/categories/icons8-diamond-ring-64.png";
+  static const String furnitureIcon = "assets/icons/categories/icons8-bed-64.png";
+  static const String animalIcon = "assets/icons/categories/icons8-cat-64.png";
+  static const String shoeIcon = "assets/icons/categories/icons8-shoe-64.png";
+  static const String cosmeticshoeIcon = "assets/icons/categories/icons8-lipstick-64.png";
 }
