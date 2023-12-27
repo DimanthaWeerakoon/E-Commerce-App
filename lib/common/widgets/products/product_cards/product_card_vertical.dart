0764805667d1f227b4cc0279f25ba7b30e2a4f25
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-27 23:57:45 +0530

    Product Vertical Card fully created

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -15,66 +15,123 @@ class EProductCardVertical extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = EHelperFunctions.isDarkMode(context);
-    return Container(
-      width: 180,
-      padding: const EdgeInsets.all(1),
-      decoration: BoxDecoration(
-          boxShadow: [EShadowStyle.verticalProductShadow],
-          borderRadius: BorderRadius.circular(ESizes.productImageRadius),
-          color: dark ? EColors.darkerGrey : EColors.white
-          ),
-      child: Column(
-        children: [
-          /// Thumbnail, Wishlist button, Discount tag
-          ERoundedContainer(
-              height: 180,
-              padding: const EdgeInsets.all(ESizes.sm),
-              backgroundColor: dark ? EColors.dark : EColors.light,
-              child: Stack(
-                children: [
-                  // -- Thumbnail image
-                  const ERoundedImage(
-                    imageURL: EImages.productItem1,
-                    applyImageRadius: true,
-                  ),
-                  // -- Sale Tag
-                  ERoundedContainer(
-                    radius: ESizes.sm,
-                    backgroundColor: EColors.secondary.withOpacity(0.8),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: ESizes.sm,
-                      vertical: ESizes.xs,
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 180,
+        padding: const EdgeInsets.all(1),
+        decoration: BoxDecoration(
+            boxShadow: [EShadowStyle.verticalProductShadow],
+            borderRadius: BorderRadius.circular(ESizes.productImageRadius),
+            color: dark ? EColors.darkerGrey : EColors.white),
+        child: Column(
+          children: [
+            /// Thumbnail, Wishlist button, Discount tag
+            ERoundedContainer(
+                height: 180,
+                padding: const EdgeInsets.all(ESizes.sm),
+                backgroundColor: dark ? EColors.dark : EColors.light,
+                child: Stack(
+                  children: [
+                    // -- Thumbnail image
+                    const ERoundedImage(
+                      imageURL: EImages.productItem1,
+                      applyImageRadius: true,
                     ),
-                    child: Text(
-                      "25%",
-                      style: Theme.of(context)
-                          .textTheme
-                          .labelLarge!
-                          .apply(color: EColors.black),
+                    // -- Sale Tag
+                    ERoundedContainer(
+                      radius: ESizes.sm,
+                      backgroundColor: EColors.secondary.withOpacity(0.8),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: ESizes.sm,
+                        vertical: ESizes.xs,
+                      ),
+                      child: Text(
+                        "25%",
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge!
+                            .apply(color: EColors.black),
+                      ),
                     ),
-                  ),
-
-                  // -- Favourite Icon Button
-                  const Positioned(
-                      top: 0,
-                      right: 0,
-                      child: ECircularIcon(
-                        icon: Iconsax.heart5,
-                        color: Colors.red,
-                      ))
-                ],
-              )),
-
-          ///  Details
-          const Padding(
-            padding: EdgeInsets.only(left: ESizes.sm),
-            child: Column(
-              children: [
-                EProductTitleText(title: 'Blue Nike Shoe')
-              ],
+    
+                    // -- Favourite Icon Button
+                    const Positioned(
+                        top: 0,
+                        right: 0,
+                        child: ECircularIcon(
+                          icon: Iconsax.heart5,
+                          color: Colors.red,
+                        ))
+                  ],
+                )),
+            const SizedBox(
+              height: ESizes.spaceBtwItems / 2,
             ),
-          )
-        ],
+    
+            ///  Details
+            Padding(
+              padding: const EdgeInsets.only(left: ESizes.sm),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const EProductTitleText(title: 'Blue Nike Shoe'),
+                  const SizedBox(
+                    height: ESizes.spaceBtwItems / 2,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Nike',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                      const SizedBox(
+                        width: ESizes.xs,
+                      ),
+                      const Icon(
+                        Iconsax.verify5,
+                        color: EColors.primary,
+                        size: ESizes.iconXs,
+                      )
+                    ],
+                  ),
+    
+                  // const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      /// Price
+                      Text(
+                        '\$45.00',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                            color: EColors.dark,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(ESizes.cardRadiusMd),
+                                bottomLeft:
+                                    Radius.circular(ESizes.productImageRadius))),
+                        child: const SizedBox(
+                            width: ESizes.iconLg * 1.2,
+                            height: ESizes.iconLg * 1.2,
+                            child: Center(
+                                child: Icon(
+                              Iconsax.add,
+                              color: EColors.white,
+                            ))),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
