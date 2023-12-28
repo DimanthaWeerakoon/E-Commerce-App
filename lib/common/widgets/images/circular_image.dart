6a66cb5c8537c87997e3eb891d5ab95dc05608dc
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-28 22:41:29 +0530

    CIRCULAR IMAGE WIDGET CREATED

---
+++ lib/common/widgets/images/circular_image.dart
@@ -0,0 +1,43 @@
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class ECircularImage extends StatelessWidget {
+  const ECircularImage({
+    super.key,
+    required this.dark,
+    this.fit = BoxFit.cover,
+    required this.image,
+    this.isNetworkImage = false,
+    this.overlayColor,
+    this.backgroundColor,
+    this.width = 56,
+    this.height = 56,
+    this.padding = ESizes.sm,
+  });
+
+  final bool dark;
+  final BoxFit? fit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? overlayColor;
+  final Color? backgroundColor;
+  final double width, height, padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color: backgroundColor ?? (dark ? EColors.black : EColors.white),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Image(
+          fit: fit,
+          image: isNetworkImage ? NetworkImage(image) : AssetImage(image) as ImageProvider,
+          color: overlayColor),
+    );
+  }
+}
--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,7 +1,11 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_app/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:e_commerce_app/common/widgets/images/circular_image.dart';
 import 'package:e_commerce_app/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
@@ -33,23 +37,49 @@ class StoreScreen extends StatelessWidget {
                   automaticallyImplyLeading: false,
                   backgroundColor: dark ? EColors.black : EColors.white,
                   expandedHeight: 440,
-                  flexibleSpace:
-                      Padding(padding: const EdgeInsets.all(ESizes.defaultSpace), 
+                  flexibleSpace: Padding(
+                      padding: const EdgeInsets.all(ESizes.defaultSpace),
                       child: ListView(
                         shrinkWrap: true,
                         physics: const NeverScrollableScrollPhysics(),
-                        children: const [
+                        children: [
                           // -- Search Bar
-                          SizedBox(height: ESizes.spaceBtwItems,),
-                          ESearchContainer(text: 'Search in Store', showBorder: true, showBackground: false, padding: EdgeInsets.zero,),
-                          SizedBox(height: ESizes.spaceBtwSections,),
+                          const SizedBox(
+                            height: ESizes.spaceBtwItems,
+                          ),
+                          const ESearchContainer(
+                            text: 'Search in Store',
+                            showBorder: true,
+                            showBackground: false,
+                            padding: EdgeInsets.zero,
+                          ),
+                          const SizedBox(
+                            height: ESizes.spaceBtwSections,
+                          ),
+
+                          // -- Featured Brands
+                          ESectionHeading(
+                              title: 'Featured Brands', onPressed: () {}),
+                          const SizedBox(
+                            height: ESizes.spaceBtwItems / 1.5,
+                          ),
+
+                          ERoundedContainer(
+                            padding: const EdgeInsets.all(ESizes.sm),
+                            showBorder: true,
+                            backgroundColor: Colors.transparent,
+                            child: Row(children: [
+                              // -- Icon
+                            ECircularImage(dark: dark)
+                            ]),
+                          )
                         ],
-                      )
-                      
-                      ),
+                      )),
                 )
               ];
             },
             body: Container()));
   }
 }
+
+
