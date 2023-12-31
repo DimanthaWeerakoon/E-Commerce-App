3c93effd4a38d411498f2a0c01cd72d42def0806
DimanthaWeerakoon <dweerakoon71@gmail.com>
2023-12-31 21:21:17 +0530

    Wishlist UI completed

--- lib/features/shop/screens/wishlist/wishlist.dart
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -1,6 +1,9 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/icons/circular_icon.dart';
+import 'package:e_commerce_app/common/widgets/layouts/grid_layout.dart';
+import 'package:e_commerce_app/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:e_commerce_app/features/shop/screens/home/home.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -12,13 +15,25 @@ class Favouritescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: EAppbar(
-          title: Text(
-        'Wishlist',
-        style: Theme.of(context).textTheme.headlineMedium,
+        title: Text(
+          'Wishlist',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        actions: [
+          ECircularIcon(
+            icon: Iconsax.add,
+            onPressed: () => Get.to(const HomeScreen()),
+          )
+        ],
       ),
-      actions: [
-        ECircularIcon(icon: Iconsax.add, onPressed: () => Get.to(const HomeScreen()),)
-      ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(ESizes.defaultSpace),
+          child: Column(children: [
+            EGridLayout(itemCount: 4, itemBuilder: (_, index) => const EProductCardVertical())
+
+          ]),
+          ),
       ),
     );
   }
