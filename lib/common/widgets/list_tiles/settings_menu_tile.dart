f39759812adb38c4f44f17a72930d6bb5c9e14ac
DimanthaWeerakoon <dweerakoon71@gmail.com>
2024-01-01 00:00:11 +0530

    Settings UI Completed

--- lib/common/widgets/list_tiles/settings_menu_tile.dart
+++ lib/common/widgets/list_tiles/settings_menu_tile.dart
@@ -2,15 +2,17 @@ import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class SettingsMenuTile extends StatelessWidget {
-  const SettingsMenuTile({super.key, required this.icon, required this.title, required this.subTitle, this.trailling});
+  const SettingsMenuTile({super.key, required this.icon, required this.title, required this.subTitle, this.trailling, this.onPressed});
 
   final IconData icon;
   final String title, subTitle;
   final Widget? trailling;
-  
+  final VoidCallback? onPressed;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: onPressed,
       leading: Icon(icon, size: 28, color: EColors.primary,),
       title: Text(title, style: Theme.of(context).textTheme.titleMedium,), 
       subtitle: Text(subTitle, style: Theme.of(context).textTheme.labelMedium,), 
--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:e_commerce_app/common/widgets/images/circular_image.dart';
+import 'package:e_commerce_app/common/widgets/list_tiles/settings_menu_tile.dart';
 import 'package:e_commerce_app/common/widgets/list_tiles/user_profile_tile.dart';
 import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
@@ -46,16 +47,103 @@ class SettingsScreen extends StatelessWidget {
             )),
 
             // --  Body
-            const Padding(
-              padding: EdgeInsets.all(ESizes.defaultSpace),
+            Padding(
+              padding: const EdgeInsets.all(ESizes.defaultSpace),
               child: Column(
                 children: [
-                  // -- Accoumt title
-                  ESectionHeading(title: 'Account Setting'),
-                  SizedBox(height: ESizes.spaceBtwItems,),
+                  // -- Account Settings
+                  const ESectionHeading(
+                    title: 'Account Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: ESizes.spaceBtwItems,
+                  ),
+
+                  SettingsMenuTile(
+                    icon: Iconsax.safe_home,
+                    title: 'My Address',
+                    subTitle: 'Set Shopping Delivery Address',
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.shopping_cart,
+                    title: 'My Cart',
+                    subTitle: 'Add, remove products and move to checkout',
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.bag_tick,
+                    title: 'My Orders',
+                    subTitle: 'In-progress andCompleted orders',
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.bank,
+                    title: 'Bank Account',
+                    subTitle: 'Withdraw balance to registered bank account',
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.discount_shape,
+                    title: 'My Coupons',
+                    subTitle: 'List of all the discounted coupons',
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.notification,
+                    title: 'Notifications',
+                    subTitle: 'Set any kind of notifications',
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.security_card,
+                    title: 'Account Privacy',
+                    subTitle: 'Manage data usage and connected accounts',
+                    onPressed: () {},
+                  ),
+
+                  // -- App Settings
+                  const SizedBox(
+                    height: ESizes.spaceBtwSections,
+                  ),
+                  const ESectionHeading(
+                    title: 'App Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: ESizes.spaceBtwItems,
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.document_upload,
+                    title: 'Local Data',
+                    subTitle: 'Upload Data to your Cloud Firebase',
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.location,
+                    title: 'Geolocation',
+                    subTitle: 'Set recommendation based on location',
+                    trailling: Switch(value: true, onChanged: (value) {}),
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.security_user,
+                    title: 'Safe MOde',
+                    subTitle: 'Search result is safe for all ages',
+                    trailling: Switch(value: false, onChanged: (value) {}),
+                    onPressed: () {},
+                  ),
+                  SettingsMenuTile(
+                    icon: Iconsax.image,
+                    title: 'HD Image Quality',
+                    subTitle: 'Set image quality to be seen',
+                    trailling: Switch(value: false, onChanged: (value) {}),
+                    onPressed: () {},
+                  ),
                 ],
               ),
-              )
+            )
           ],
         ),
       ),
