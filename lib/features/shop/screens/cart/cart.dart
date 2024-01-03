893719aa86f1136762d61c33ccdd14ae51efb0c3
DimanthaWeerakoon <dweerakoon71@gmail.com>
2024-01-03 21:50:09 +0530

    Cart UI Completed

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,13 +1,9 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
-import 'package:e_commerce_app/common/widgets/icons/circular_icon.dart';
 import 'package:e_commerce_app/common/widgets/products/cart/add_remove_button.dart';
 import 'package:e_commerce_app/common/widgets/products/cart/cart_item.dart';
 import 'package:e_commerce_app/common/widgets/texts/product_price_text.dart';
-import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
-import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -19,40 +15,44 @@ class CartScreen extends StatelessWidget {
           showBackArrow: true,
           title: Text('Cart'),
         ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(ESizes.defaultSpace),
-          child: ListView.separated(
-            shrinkWrap: true,
-            itemCount: 4,
-            separatorBuilder: (_, __) => const SizedBox(
-              height: ESizes.spaceBtwSections,
-            ),
-            itemBuilder: (_, index) => const Column(
-              children: [
-                ECartItem(),
-                SizedBox(
-                  height: ESizes.spaceBtwItems,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 75,
-                        ),
-
-                        /// Add, Remove buttons
-                        EProductQuantityWithAddAndRemovebutton(),
-                      ],
-                    ),
-                    
-                    EProductPriceText(price: '266', isLarge: true,)
-                  ],
-                )
-              ],
-            ),
+        body: ListView.separated(
+        padding: const EdgeInsets.all(ESizes.defaultSpace),
+          shrinkWrap: true,
+          itemCount: 8,
+          separatorBuilder: (_, __) => const SizedBox(
+            height: ESizes.spaceBtwSections,
           ),
-        ));
+          itemBuilder: (_, index) => const Column(
+            children: [
+              ECartItem(),
+              SizedBox(
+                height: ESizes.spaceBtwItems,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 75,
+                      ),
+
+                      /// Add, Remove buttons
+                      EProductQuantityWithAddAndRemovebutton(),
+                    ],
+                  ),
+                  
+                  EProductPriceText(price: '266', isLarge: true,)
+                ],
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar:Padding(
+          padding: const EdgeInsets.all(ESizes.defaultSpace),
+          child: ElevatedButton(onPressed: () {}, child: Text('Checkout \$266.00'),),
+        ) ,
+        );
+        
   }
 }
