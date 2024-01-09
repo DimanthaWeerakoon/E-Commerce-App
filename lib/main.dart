b9abe79f095d4dbb242e7e4c597c80ca7562831d
DimanthaWeerakoon <dweerakoon71@gmail.com>
2024-01-09 22:54:06 +0530

    FireBase plugins added, Database created in firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:e_commerce_app/app.dart';
+import 'package:e_commerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then((value) => null);
+
   runApp(const App());
 }
